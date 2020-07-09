4ee24b4d89bea230368c6f71d3a4f8534e7adaeb
Yunqi Zhang <849520755@qq.com>
2020-07-09 20:47:17 +0300

    Create images.html to show more picture.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,3 +27,15 @@ function addRandomGreeting() {
   greetingContainer.innerText = greeting;
 }
 
+// Change style of top container on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("top").classList.add("w3-card-4", "w3-animate-opacity");
+    document.getElementById("intro").classList.add("w3-show-inline-block");
+  } else {
+    document.getElementById("intro").classList.remove("w3-show-inline-block");
+    document.getElementById("top").classList.remove("w3-card-4", "w3-animate-opacity");
+  }
+}
+
